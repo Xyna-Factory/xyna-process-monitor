b9f312307395a44d0803d8abd791db4fde166d50
johannesheucher-gip <107180330+johannesheucher-gip@users.noreply.github.com>
2023-12-14 17:33:34 +0100

    Update version.ts

--- version.ts
+++ version.ts
@@ -16,4 +16,4 @@
  * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
  */
 export const ProcessMonitorName = 'Process Monitor';
-export const ProcessMonitorVersion = '2.0.1';
+export const ProcessMonitorVersion = '2.0.2';
