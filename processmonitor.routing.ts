e3913661586175db89275646bbdf8cc696c0d223
Julian von Selasinsky <julian.vonselasinsky@outlook.de>
2024-02-12 15:55:34 +0100

    Bug fixing upgrade to Angular 16

--- processmonitor.routing.ts
+++ processmonitor.routing.ts
@@ -18,7 +18,7 @@
 import { RouterModule } from '@angular/router';
 
 import { XynaRoutes } from '@zeta/nav';
-import { RightGuardCanActivate } from '@zeta/nav/right.guard';
+import { rightGuardCanActivate } from '@zeta/nav/right.guard';
 
 import { RIGHT_PROCESS_MONITOR } from './const';
 import { ProcessmonitorComponent } from './processmonitor.component';
@@ -36,7 +36,7 @@ export const ProcessmonitorRoutes: XynaRoutes = [
     {
         path: root,
         component: ProcessmonitorComponent,
-        canActivate: [RightGuardCanActivate],
+        canActivate: [rightGuardCanActivate],
         data: {
             right: RIGHT_PROCESS_MONITOR,
             reuse: root,
