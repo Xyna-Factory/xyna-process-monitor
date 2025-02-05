933cd5e18d5e720a6879006a7bf0cffc84ba9358
Julian von Selasinsky <julian.vonselasinsky@outlook.de>
2025-02-05 12:29:49 +0100

    update version

--- version.ts
+++ version.ts
@@ -16,4 +16,4 @@
  * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
  */
 export const ProcessMonitorName = 'Process Monitor';
-export const ProcessMonitorVersion = '2.2.0';
+export const ProcessMonitorVersion = '2.3.0';
