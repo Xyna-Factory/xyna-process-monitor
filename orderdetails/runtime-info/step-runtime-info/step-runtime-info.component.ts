bb78cadd23319d4e602153a16171f17524740757
jvs-gip <julian.vonselasinsky@gip.com>
2026-09-08 15:58:10 +0200

    Migration to signal queries

--- orderdetails/runtime-info/step-runtime-info/step-runtime-info.component.ts
+++ orderdetails/runtime-info/step-runtime-info/step-runtime-info.component.ts
@@ -15,7 +15,7 @@
  * limitations under the License.
  * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
  */
-import { ChangeDetectionStrategy, AfterContentChecked, Component, inject, Input, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, AfterContentChecked, Component, inject, Input, viewChild } from '@angular/core';
 
 import { ApiService, Xo, XoArray, XoDescriberCache, XoObject, XoStructureObject } from '@zeta/api';
 import { copyToClipboard, isArray } from '@zeta/base';
@@ -53,14 +53,11 @@ export class StepRuntimeInfoComponent implements AfterContentChecked {
     private readonly dataSources: XcReadonlyStructureTreeDataSource[];
     private readonly structureCache = new XoDescriberCache<XoStructureObject>();
 
-    @ViewChild('treeIn', { read: XcReadonlyTreeComponent, static: false })
-    treeIn: XcReadonlyTreeComponent;
+    readonly treeIn = viewChild('treeIn', { read: XcReadonlyTreeComponent });
 
-    @ViewChild('treeOut', { read: XcReadonlyTreeComponent, static: false })
-    treeOut: XcReadonlyTreeComponent;
+    readonly treeOut = viewChild('treeOut', { read: XcReadonlyTreeComponent });
 
-    @ViewChild('treeErr', { read: XcReadonlyTreeComponent, static: false })
-    treeErr: XcReadonlyTreeComponent;
+    readonly treeErr = viewChild('treeErr', { read: XcReadonlyTreeComponent });
 
     private _lazyLoadingLimit: number;
     private _runtimeInfoOrderId: string;
@@ -112,10 +109,10 @@ export class StepRuntimeInfoComponent implements AfterContentChecked {
             return;
         }
         // expand trees, if necessary
-        [this.treeIn, this.treeOut, this.treeErr]
+        [this.treeIn(), this.treeOut(), this.treeErr()]
             .forEach((tree, t) => tree?.dataSource.structureTreeData
                 .filter((_, n) => this.expandTree.has(this.getExpandTreeKey(t, n)))
-                .forEach((node, n) => tree.items
+                .forEach((node, n) => tree.items()
                     .filter(item => item.node === node)
                     .forEach(item => {
                         this.expandTree.delete(this.getExpandTreeKey(t, n));
